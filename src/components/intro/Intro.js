fc04fd720f0b7538d1dfa6e085dd24eaa0d441f4
Marian Francescu <marian.francescu98@gmail.com>
2021-05-22 01:05:46 +0300

    Almost finished intro page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,9 +1,25 @@
 import './intro.scss'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/myPhoto.jpg" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Marian Francescu</h1>
+                    <h3>Student</h3>
+                    <a href="#portfolio">
+                    <ExpandMoreIcon className="arrow" />
+                </a>
+                </div>
+                
+            </div>
         </div>
     )
 }
