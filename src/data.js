43021dbeed82281eed3305a9164a622a8d5d0efa
Marian Francescu <marian.francescu98@gmail.com>
2021-05-22 10:58:14 +0300

    Added slider in the works component

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const featuredPortfolio = [
+    {
+      id: 1,
+      title: "PC-Builder",
+      img:
+        "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 2,
+      title: "Messenger",
+      img:
+        "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 3,
+      title: "Car parts shop",
+      img:
+        "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 4,
+      title: "Parking Application",
+      img:
+        "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+  ];
+  
+  export const webPortfolio = [
+    {
+      id: 1,
+      title: "PC-Builder",
+      img: "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 2,
+      title: "Messenger",
+      img: "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 3,
+      title: "Car parts shop",
+      img: "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+    {
+      id: 4,
+      title: "Virtual Running Platform - NESS - Liga AC Labs",
+      img: "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+  ];
+  
+  export const desktopPortfolio = [
+    {
+      id: 1,
+      title: "Parking Application",
+      img:
+        "https://user-images.githubusercontent.com/28231703/118353753-5f9cac80-b570-11eb-8207-1fd438ca0671.png",
+    },
+  ];
